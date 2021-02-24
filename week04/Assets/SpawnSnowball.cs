82607fa430f6ce199a271185c4e3c40fc9395f70
CoralineDark <49164730+CoralineDark@users.noreply.github.com>
2021-02-24 01:48:35 -0500

    snowballFixed

---
+++ week04/Assets/SpawnSnowball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSnowball : MonoBehaviour
+{
+    public GameObject snowballPrefab; 
+    private GameObject cameraObj; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraObj = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { 
+            GameObject snowball = Instantiate(snowballPrefab, transform.position, transform.rotation);
+            snowball.transform.LookAt(cameraObj.transform);
+            //GameObject snowball = Instantiate(snowballPrefab, transform.position, transform.rotation); 
+            //Vector3 vectorToTarget = cameraObj.transform.position - transform.position;
+            //vectorToTarget = vectorToTarget.normalized;
+            //snowball.transform.forward = vectorToTarget; 
+        }
+    }
+}
