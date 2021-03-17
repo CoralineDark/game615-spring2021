ee4ae57a3c39f46e010774b6f160d80fef98a6b8
CoralineDark <49164730+CoralineDark@users.noreply.github.com>
2021-03-17 12:04:39 -0400

    more small updates

--- mc7998a_Official_Project1/Assets/movePlayer.cs
+++ mc7998a_Official_Project1/Assets/movePlayer.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class movePlayer : MonoBehaviour
 {
+
+    public float speed; 
+    public float clockwise; 
+    public float counterClockwise; 
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +15,26 @@ public class movePlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() { 
+        Rigidbody rb = GetComponent <Rigidbody>(); 
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow )) {
+            rb.velocity=transform.forward *speed; 
+        }
+        else if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow )) {
+             rb.velocity=transform.forward *0; 
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow )) {
+            transform.Rotate(0, Time.deltaTime * counterClockwise, 0);  
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow )) {
+             rb.velocity=-transform.forward *speed; 
+        }
+        else if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow )) {
+             rb.velocity=-transform.forward *0; 
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow )) {
+            transform.Rotate(0, Time.deltaTime * clockwise, 0); 
+        }
+
     }
 }
